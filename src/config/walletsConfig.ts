53cb9b06d746614d5686c62d921b2bbc710eb9d3
skyxcripto <producer@skynance.net>
2022-01-19 10:35:53 -0300

    fix connectors

--- src/config/walletsConfig.ts
+++ src/config/walletsConfig.ts
@@ -1,48 +1,26 @@
-import MetamaskIco from "../assets/icons/metamask.svg";
-import TrustWalletIco from "../assets/icons/trustwallet.svg";
-import MathWalletIco from "../assets/icons/mathwallet.svg";
-import TokenpocketIco from "../assets/icons/tokenpocket.svg";
-import WalletConnectIco from "../assets/icons/walletconnect.svg";
-import BinanceWalletIco from "../assets/icons/binancewallet.svg";
-import SafePalIco from "../assets/icons/safepalwallet.svg";
-import { Connectors } from "./types";
+import { Connectors } from './types'
 
 const wallets = [
-    {
-        title: "MetaMask",
-        image: MetamaskIco,
-        connector: Connectors.INJECTED
-    },
-    {
-        title: "Binance Chain Wallet",
-        image: BinanceWalletIco,
-        connector: Connectors.BSC
-    },
-    {
-        title: "Trust Wallet",
-        image: TrustWalletIco,
-        connector: Connectors.INJECTED,
-    },
-    {
-        title: "Wallet Connect",
-        image: WalletConnectIco,
-        connector: Connectors.WALLET_CONNECT,
-    },
-    {
-        title: "Math Wallet",
-        image: MathWalletIco,
-        connector: Connectors.INJECTED,
-    },
-    {
-        title: "Token Pocket",
-        image: TokenpocketIco,
-        connector: Connectors.INJECTED,
-    },
-    {
-        title: "SafePal Wallet",
-        image: SafePalIco,
-        connector: Connectors.INJECTED,
-    },
-];
+  {
+    title: 'MetaMask',
+    image: '/images/icons/metamask.svg',
+    connector: Connectors.INJECTED,
+  },
+  {
+    title: 'Binance Chain Wallet',
+    image: '/images/icons/binancewallet.svg',
+    connector: Connectors.BSC,
+  },
+  {
+    title: 'Trust Wallet',
+    image: '/images/icons/trustwallet.svg',
+    connector: Connectors.INJECTED,
+  },
+  {
+    title: 'Wallet Connect',
+    image: '/images/icons/walletconnect.svg',
+    connector: Connectors.WALLET_CONNECT,
+  },
+]
 
-export default wallets;
+export default wallets
